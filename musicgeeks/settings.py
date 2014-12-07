55fe4f09ac9caee5c8a543f76825a122bd55084c
Alex Sears <alexwsears@gmail.com>
2014-12-06 21:40:59 -0500

    Updated user agent

--- musicgeeks/settings.py
+++ musicgeeks/settings.py
@@ -1,17 +1,8 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for musicgeeks project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'musicgeeks'
 
 SPIDER_MODULES = ['musicgeeks.spiders']
 NEWSPIDER_MODULE = 'musicgeeks.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'musicgeeks (+http://www.yourdomain.com)'
+DEFAULT_ITEM_CLASS = 'musicgeeks.items.SongPage'
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'
