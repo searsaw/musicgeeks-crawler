0221e2464ac263295a7e25dfff5d6143b6aad9c1
Alex Sears <alexwsears@gmail.com>
2014-12-06 21:52:15 -0500

    Added export settings

--- musicgeeks/settings.py
+++ musicgeeks/settings.py
@@ -6,3 +6,8 @@ NEWSPIDER_MODULE = 'musicgeeks.spiders'
 DEFAULT_ITEM_CLASS = 'musicgeeks.items.SongPage'
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'
+
+# Export settings
+FEED_URI = 'file://./'
+FEED_FORMAT = 'json'
+FEED_STORE_EMPTY = True
